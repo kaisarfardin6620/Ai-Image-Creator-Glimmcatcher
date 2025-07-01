2225023af89db1289b383ab3533bdb98d2d1c544
Abdullah Kaisar Fardin <kaisarfardin128@gmail.com>
2025-07-02 01:32:29 +0600

    Update test script with new user input for image generation request

--- test_glimmcatcher.js
+++ test_glimmcatcher.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 async function testGlimmcatcher() {
   try {
     const response = await axios.post('http://127.0.0.1:5001/glimmcatcher', {
-      user_input: "can u help me make an image 'a girl with blonde hair and blue eyes sitting on a bench in a park, holding wine glass looking at the sky, 4k reslotuion 1024*1024'",
+      user_input: "hi there buddy ,design a mt 15 bike for me, it must have to be look realistic, with a good design,",
       user_id: "test_node_user"
     });
     console.log("Response from FastAPI:", response.data);
